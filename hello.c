9613186caf30a2694145b298cdc02653b0a90512
Robert Lupton the Good <rhl@astro.princeton.edu>
2012-09-24 11:56:15 -0400

    Removed that confusing define

--- hello.c
+++ hello.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-#define NITER 10
-
 int i;					/* an integer */
 const char *str = "Hello World";
 
 int
 main()
 {
-   printf("What I tell you %d times is true\n", NITER);
-   for (i = 0; i < NITER; ++i) {
+   printf("What I tell you %d times is true\n", 10);
+   for (i = 0; i < 10; ++i) {
       printf("%d %s\n", i, str);
    }
 }
