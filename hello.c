696a3221dbc0da3fbdb415109744463047eceb35
Robert Lupton the Good <rhl@astro.princeton.edu>
2012-10-01 16:26:45 -0400

    Set the number of iterations on the command line

--- hello.c
+++ hello.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
-#define NITER 10
+#include <stdlib.h>
 
 int
-main()
+main(int ac, const char **av)
 {
+   const int niter = (ac > 1) ? atoi(av[1]) : 10;
+
    const char *str = "Hello World";
-   printf("What I tell you %d times is true\n", NITER);
-   for (int i = 0; i < NITER; ++i) {
+   printf("What I tell you %d times is true\n", niter);
+   for (int i = 0; i < niter; ++i) {
       printf("%d %s\n", i, str);
    }
 }
